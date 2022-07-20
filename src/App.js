f74888d0b6f27c42e91cd9078cda637d59791980
Elio1995 <mehmetielio95@gmail.com>
2022-07-20 10:50:59 +0200

    Created components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import "./App.css";
 
-function App() {
-  const keyspads = [
-    { label: 0, color: "standard" },
-    { label: 1, color: "standard" },
-    { label: 2, color: "standard" },
-    { label: 3, color: "standard" },
-    { label: 4, color: "standard" },
-    { label: 5, color: "standard" },
-    { label: 6, color: "standard" },
+export default function App() {
+  const keypads = [
     { label: 7, color: "standard" },
     { label: 8, color: "standard" },
     { label: 9, color: "standard" },
     { label: "DEL", action: "delete", color: "mark" },
+
+    { label: 4, color: "standard" },
+    { label: 5, color: "standard" },
+    { label: 6, color: "standard" },
     { label: "+", action: "add", color: "standard" },
+
+    { label: 1, color: "standard" },
+    { label: 2, color: "standard" },
+    { label: 3, color: "standard" },
     { label: "-", action: "subtract", color: "standard" },
+
     { label: ".", action: "decimal", color: "standard" },
+    { label: 0, color: "standard" },
     { label: "/", action: "divide", color: "standard" },
     { label: "x", action: "multiply", color: "standard" },
+
     { label: "RESET", action: "reset", color: "mark", span: true },
     { label: "=", action: "calculate", color: "red", span: true },
   ];
@@ -28,5 +32,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
